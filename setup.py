a71500f91c109d9a34503e99f99e4992b3822152
sunspai-genai <genaisuns@gmail.com>
2025-01-06 15:25:44 -0500

    publish

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sunstradeapp",
+    version="0.1",
+    packages=find_packages(),
+    install_requires = [],
+    author="Sunstrade",
+    author_email="sunstrade@gmail.com"
+)
